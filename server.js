6c7b7397a86d16a859fdab09dfe6d73cd85744b3
RyukyuColin <richardsonjcolin@gmail.com>
2018-12-20 14:24:36 -0800

    body parser modify limit size

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const middlewares = require('./middlewares/middlewares.js');
 const apiRoutes = require('./routes/api');
 //middlewares
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(middlewares.checkAPIKey);
 
 // routes
